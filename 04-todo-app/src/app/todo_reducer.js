4a8e238486d1830c250e0c8d946898698bb97e91
nishant <nishant.singh87@gmail.com>
2018-01-13 20:44:16 +0530

    lesson 04 : Use redux.combineReducers

--- 04-todo-app/src/app/todo_reducer.js
+++ 04-todo-app/src/app/todo_reducer.js
@@ -1,12 +1,20 @@
 import todoListReducer from './todo_list_reducer';
 import visibilityFilterReducer from './visiblity_filer_reducer';
+import {combineReducers} from 'redux';
 
-
+/*
+ Custom implementation of combine reducer
+ ----------------------------------------
 const toddReducer = (state = {}, action)=> {
     return {
       todo: todoListReducer(state.todo, action),
       visibilityFilter: visibilityFilterReducer(state.visibilityFilter, action),
     }
 };
-
 export default toddReducer;
+*/
+
+export default combineReducers({
+  todo: todoListReducer,
+  visibilityFilter: visibilityFilterReducer,
+});
