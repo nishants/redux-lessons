3e8fbf22d4aeccb73745ec030eea404d041aaa1a
nishant <nishant.singh87@gmail.com>
2018-02-21 20:36:00 +0530

    lesson 04 : Pure functoins in reducer.

--- 04-todo-app/src/app/todo_list/todo_list_reducer.js
+++ 04-todo-app/src/app/todo_list/todo_list_reducer.js
@@ -1,58 +1,13 @@
 import TODO from './todo_state';
 
-const todoTaskReducer = (todo, action) =>{
-  switch(action.type){
-    case 'ADD_TODO':
-      return {
-        id  : action.taskID,
-        name: action.taskName,
-        complete: false
-      };
-    case 'FINISHED_TODO':
-      return {
-        ...todo,
-        complete : true
-      };
-    case 'PENDING_TODO':
-      return {
-        ...todo,
-        complete : false
-      };
-    default:
-      return todo;
-  }
-};
-//
-// const todoListReducer = (todoList=[], action)=>{
-//   switch(action.type){
-//     case 'ADD_TODO' :
-//       return todoList.concat(todoTaskReducer(undefined, action));
-//     case 'FINISHED_TODO':
-//       return todoList.map((task)=> {
-//         return task.id === action.taskID ? todoTaskReducer(task, action) : task
-//       });
-//     case 'PENDING_TODO':
-//       return todoList.map((task)=> {
-//         return task.id === action.taskID ? todoTaskReducer(task, action) : task
-//       });
-//     default :
-//       return todoList;
-//   }
-// };
-
-
 const todoListReducer = (state=TODO.initialState(), action)=>{
   switch(action.type){
     case 'ADD_TODO' :
       return TODO.addTask(state, action.taskName )
     case 'FINISHED_TODO':
-      return state.map((task)=> {
-        return task.id === action.taskID ? todoTaskReducer(task, action) : task
-      });
+      return TODO.finishTask(state, action.taskID );
     case 'PENDING_TODO':
-      return state.map((task)=> {
-        return task.id === action.taskID ? todoTaskReducer(task, action) : task
-      });
+      return TODO.setToPending(state, action.taskID );
     default :
       return state;
   }
--- 04-todo-app/src/app/todo_list/todo_state.js
+++ 04-todo-app/src/app/todo_list/todo_state.js
@@ -1,7 +1,15 @@
-export default {
+let TODO = {
   initialState: () => [],
-  addTask: (state, name)=> state.concat({
-    id: state.length,
-    name: name
-  })
-}
+
+  addTask: (state, name)=> state.concat({id: state.length, name: name}),
+
+  finishTask: (state, taskId) => (
+      state.map(task=> task.id === taskId ? {...task, complete: true} : task)
+  ),
+
+  setToPending: (state, taskId) => (
+      state.map(task=> task.id === taskId ? {...task, complete: false} : task)
+  )
+};
+
+export default TODO
